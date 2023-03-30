60553d5c3f8d7f1810ecb3bd84db12ce03ea37ef
michaelplynch <89206184+michaelplynch@users.noreply.github.com>
2023-03-30 17:16:03 +0100

    updated unit testing, added bioconductor installation instructions to vignette, added seed() to reassign() in vignette

--- tests/testthat/test-subset_vcf.R
+++ tests/testthat/test-subset_vcf.R
@@ -1,6 +1,14 @@
+data(sce,vcf, package = "demuxSNP")
+
 test_that("Only accept character genes", {
-    data(vcf, package = "demuxSNP")
     top_genes <- seq_len(5)
     ensdb <- EnsDb.Hsapiens.v86::EnsDb.Hsapiens.v86
     expect_error(subset_vcf(vcf, top_genes, ensdb))
+})
+
+test_that("Check reproducibility in vcf subsetting", {
+  top_genes <- common_genes(sce)
+  ensdb <- EnsDb.Hsapiens.v86::EnsDb.Hsapiens.v86
+  vcf_sub<-subset_vcf(vcf, top_genes, ensdb)
+  expect_equal(dim(vcf_sub)[1],2399)
 })
